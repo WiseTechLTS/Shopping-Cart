02e40fac609c85d1fca7ae59d00804360c7afd26
Kyle Wisecarver <wiseworks.2022@gmail.com>
2023-03-15 06:14:19 -0500

    working application

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { Routes, Route } from "react-router-dom";
-import { Container } from "react-bootstrap";
-import { Home } from "./pages/Home";
-import { Store } from "./pages/Store";
-import { About } from "./pages/About";
-import { Navbar } from "./components/Navbar";
-import { ShoppingCartProvider } from "./context/ShoppingCartContext";
-
+import { Routes, Route } from "react-router-dom"
+import { Container } from "react-bootstrap"
+import { Home } from "./pages/Home"
+import { Store } from "./pages/Store"
+import { About } from "./pages/About"
+import { Navbar } from "./components/Navbar"
+import { ShoppingCartProvider } from "./context/ShoppingCartContext"
+import './styles.css';
 function App() {
   return (
     <ShoppingCartProvider>
@@ -18,7 +18,7 @@ function App() {
         </Routes>
       </Container>
     </ShoppingCartProvider>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,14 @@
-import { Button, Container, Nav, Navbar as NavbarBs } from "react-bootstrap";
-import { NavLink } from "react-router-dom";
-import { useShoppingCart } from "../context/ShoppingCartContext";
+import { Button, Container, Nav, Navbar as NavbarBs } from "react-bootstrap"
+import { NavLink } from "react-router-dom"
+import { useShoppingCart } from "../context/ShoppingCartContext"
 
 export function Navbar() {
-  const { openCart, cartQuantity } = useShoppingCart();
+  const { openCart, cartQuantity } = useShoppingCart()
   return (
-    <NavbarBs sticky="top" className="bg-white shadow-sm mb-3">
-      <Container>
+    <NavbarBs sticky="top" className="shadow-sm mb-3">
+      <Container style={{
+        backgroundColor: 'white',
+      }}>
         <Nav className="me-auto">
           <Nav.Link to="/" as={NavLink}>
             Home
@@ -51,5 +53,5 @@ export function Navbar() {
         )}
       </Container>
     </NavbarBs>
-  );
+  )
 }
